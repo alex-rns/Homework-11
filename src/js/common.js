2a3d6d91c1b669137507284b7d3d863b92245afc
alex-rns <alexrns3@gmail.com>
2017-12-14 18:11:40 +0200

    +nav

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,9 @@
-$(function() {
+$(document).ready(function() {
+    $('.nav-item').on('mouseover', function () {
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+        $('.tab-item').removeClass('d-block');
+        $(this).find($('.tab-item')).addClass('d-block');
 
-	// Custom JS
-
+    })
 });
